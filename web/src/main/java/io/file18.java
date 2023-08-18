faaa9bdcbdd060c1dadb623426e88d40514dc0f7
sagong0 <1w2q3e@naver.com>
2023-08-18 18:18:48 +0900

    js

--- web/src/main/java/io/file18.java
+++ web/src/main/java/io/file18.java
@@ -1,4 +1,12 @@
 package io;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 // HOMEWORK
 /*
  * Scanner를 이용하여 총 10개의 숫자를 입력합니다.
@@ -14,7 +22,53 @@ package io;
 public class file18 {
 
 	public static void main(String[] args) {
-
+		try {
+			data18 a = new data18();
+			a.abc();
+		}catch(NumberFormatException e) {
+			System.out.println(e);
+		} 
+		catch (Exception e) {
+			System.out.println("error");
+			System.out.println(e);
+		}
 	}
 
+}
+
+class data18{
+	public void abc() throws Exception{
+		//Files.createFile(Paths.get("E:\\project\\web\\src\\main\\webapp\\data_num.txt"));
+		String url = "E:\\project\\web\\src\\main\\webapp\\data_num.txt";
+		
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(url)));
+		
+		int a = 0;
+
+		while(true) {
+			System.out.println("1~100 까지의 숫자 중 한가지를 입력하세요");
+			String data = sc.nextLine().intern();
+			
+			if(data == "quit") {
+				System.out.println("종료 됩니다.");
+				break;
+			}else if(a >= 10) {
+				System.out.println("10개 꽉찼습니다.");
+				break;
+			}
+			else{
+				if(Integer.parseInt(data) > 100 || Integer.parseInt(data) <= 0) {
+					System.out.println("0 ~ 100까지의 숫자만 입력 가능합니다.");
+				}
+				else {
+					bw.write(data + "\n");
+					++a;
+					bw.flush();
+				}
+			}
+		}
+		bw.close();
+		sc.close();
+	}
 }
